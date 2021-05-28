08274696a22d3f6024f4b24fb5dc6b7d85180099
Musthafa Faishal <musthafafaishal@gmail.com>
2021-05-28 14:28:14 +0700

    Edit views

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,15 +9,16 @@
   <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="../../public/urbanui-login-template-free-2-38baaa875b8e/assets/css/login.css">
+  <link rel="stylesheet" href="<?= base_url('urbanui-login-template-free-2-38baaa875b8e/assets/css/login.css'); ?>">
 </head>
+
 <body>
   <main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
     <div class="container">
       <div class="card login-card">
         <div class="row no-gutters">
           <div class="col-md-5">
-            <img src="../../public/urbanui-login-template-free-2-38baaa875b8e/assets/images/pr.jpg" alt="login" class="login-card-img">
+            <img src="<?= base_url('urbanui-login-template-free-2-38baaa875b8e/assets/images/pr.jpg'); ?>" alt="login" class="login-card-img">
           </div>
           <div class="col-md-7">
             <div class="card-body">
@@ -25,22 +27,22 @@
               </div>
               <p class="login-card-description">Login</p>
               <form action="#!">
-                  <div class="form-group">
-                    <label for="email" class="sr-only">Email</label>
-                    <input type="email" name="email" id="email" class="form-control" placeholder="Email address">
-                  </div>
-                  <div class="form-group mb-4">
-                    <label for="password" class="sr-only">Password</label>
-                    <input type="password" name="password" id="password" class="form-control" placeholder="***********">
-                  </div>
-                  <input name="login" id="login" class="btn btn-block login-btn mb-4" type="button" value="Login">
-                </form>
-                <a href="#!" class="forgot-password-link">Forgot password?</a>
-                <p class="login-card-footer-text">Don't have an account? <a href="#!" class="text-reset">Register here</a></p>
-                <nav class="login-card-footer-nav">
-                  <a href="#!">Terms of use.</a>
-                  <a href="#!">Privacy policy</a>
-                </nav>
+                <div class="form-group">
+                  <label for="email" class="sr-only">Email</label>
+                  <input type="email" name="email" id="email" class="form-control" placeholder="Email address">
+                </div>
+                <div class="form-group mb-4">
+                  <label for="password" class="sr-only">Password</label>
+                  <input type="password" name="password" id="password" class="form-control" placeholder="***********">
+                </div>
+                <input name="login" id="login" class="btn btn-block login-btn mb-4" type="button" value="Login">
+              </form>
+              <a href="#!" class="forgot-password-link">Forgot password?</a>
+              <p class="login-card-footer-text">Don't have an account? <a href="#!" class="text-reset">Register here</a></p>
+              <nav class="login-card-footer-nav">
+                <a href="#!">Terms of use.</a>
+                <a href="#!">Privacy policy</a>
+              </nav>
             </div>
           </div>
         </div>
@@ -77,4 +79,5 @@
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </body>
+
 </html>
